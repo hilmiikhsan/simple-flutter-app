7290096d6cddb05fb8ba826ba5c694dc48498baa
hilmiikhsan <ikhsanhilmimuhammad@gmail.com>
2024-11-21 04:52:49 +0700

    feat: add splash screen

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, "/login");
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("assets/images/shoes-1.png", width: 100, height: 100),
+            const CircularProgressIndicator()
+          ],
+        ),
+      ),
+    );
+  }
+}
